79da6f8b260b8c4f5794bf0296104aa5779c0d3f
criedley <creedley@gmail.com>
2022-11-29 18:56:30 -0500

    made changes

--- facial-recognition.py
+++ facial-recognition.py
@@ -1,5 +1,11 @@
-#!pip install deepface
 from deepface import DeepFace
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
 
-DeepFace.stream("database")
+df = DeepFace.find(
+    img_path = "img1.png", 
+    db_path = "C:\python-projects\ece-523-final-proj\database",
+    )
+
+print(df.head())
