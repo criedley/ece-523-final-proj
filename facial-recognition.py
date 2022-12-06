61e5b9f4f52728d483a5387a087b9f72220bcc61
criedley <creedley@gmail.com>
2022-12-06 13:56:54 -0500

    wrapped code in function. added comments

--- facial-recognition.py
+++ facial-recognition.py
@@ -7,85 +7,50 @@ import pandas as pd
 import numpy as np
 import cv2
 
-# model =VGGFace.loadModel
+def getFaces(img_path):
 
-# return dict for each face in image
-face_dict = RetinaFace.detect_faces(img_path = "img1.png")
+    """
+    input: exact path to image
+    output: list of size N-by-5, containing four image coords representing bounding box of face,
+    and a string representing the closest identity in the database for each detected face. 
+    """
+    # create dictionary to store facial regions
+    face_dict = RetinaFace.detect_faces(img_path)
 
-face_df = pd.DataFrame.from_dict(face_dict).T
+    # convert dict to pandas dataframe
+    face_df = pd.DataFrame.from_dict(face_dict).T
 
-# print(face_df)
-# print(face_df['facial_area'][0][0])
+    # store group image as numpy array
+    img = cv2.imread(img_path)
 
-img = cv2.imread("img1.png")
-# rect = face_df['facial_area'][0]
+    output = []
+    faces = []
+
+    # loop over detected faces
+    for ind in face_df.index:
+        # get bounding boxes for each face
+        rect = face_df['facial_area'][ind]
+
+        # extract faces by getting sub image for each face from group imgage
+        ROI = img[rect[1]:rect[3], rect[0]:rect[2]]
+        faces.append(ROI)
+        output.append(rect)
 
 
-output = {}
+    BGRfaces = []
 
-faces = []
+    for face in faces:
+        # convert sub image to numpy array, convert to BGR
+        BGRfaces.append(cv2.cvtColor(np.array(face), cv2.COLOR_RGB2BGR))
 
-for ind in face_df.index:
-    rect = face_df['facial_area'][ind]
-    ROI = img[rect[1]:rect[3], rect[0]:rect[2]]
-    faces.append(ROI)
+    for i in range(len(BGRfaces)):
+        df = DeepFace.find(img_path = BGRfaces[i], db_path= "C:\python-projects\ece-523-final-proj\database", enforce_detection = False, silent = "True", prog_bar = "False")
+        id_path = df.iloc[0].identity
+        norm_path = os.path.normpath(id_path)
+        split_path = norm_path.split(os.sep)
+
+        # index of split path is name of id dir in database, e.g. root\my-project\database\<id>\myimg.png
+        id = split_path[4]
+        output[i].append(id)
     
-    
-
-# faces = RetinaFace.extract_faces(img_path = "img1.png", align = True)
-
-BGRfaces = []
-
-for face in faces:
-    BGRfaces.append(cv2.cvtColor(np.array(face), cv2.COLOR_RGB2BGR))
-
-# plt.imshow(BGRfaces[6])
-# plt.show()
-
-
-for i in range(len(BGRfaces)):
-    df = DeepFace.find(img_path = BGRfaces[i], db_path= "database", enforce_detection = False, silent = "True", prog_bar = "False")
-    id_path = df.iloc[0].identity
-    norm_path = os.path.normpath(id_path)
-    split_path = norm_path.split(os.sep)
-
-    # index of split path is name of id dir in database, e.g. root\my-project\database\<id>\myimg.png
-    id = split_path[4]
-
-print(output)   
-
-
-
-
-
-    #  facial_area = identity["facial_area"]
-
-    # for dir in split_path:
-    #     if dir == "Ahmed-A":
-    #         id = 0
-    #     elif dir == "s1":
-    #         id = 1
-    #     elif dir == "s2":
-    #         id = 2
-    #     elif dir == "s3":
-    #         id = 3
-    #     elif dir == "s4":
-    #         id = 4
-    #     elif dir == "s5":
-    #         id = 5
-    #     elif dir == "s6":
-    #         id = 6
-    #     elif dir == "s7":
-    #         id = 7
-
-    #     print(id)
-
-# obj = RetinaFace.detect_faces("img1.png")
-
-# print(len(obj.keys()))
-
-# for key in obj.keys():
-#     identity = obj[key]
-#     print(identity)
-
-#     facial_area = identity["facial_area"]
+    return output
